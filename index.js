42da83b8027cd9b1596995a0ed6feb4d46032285
Buenomanu <129122996+Buenomanu@users.noreply.github.com>
2024-08-12 23:09:18 +0000

    12/08/24

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const idade = 17;
-cost anoNascimento = 2007;
+let idade = 17;
+const anoNascimento = 2007;
 let anoAtual = 2024;
+const nomeCompleto = "Manuela Bueno";
+
+console.log("Meu nome é " + nomeCompleto  + " nascida no ano de " + anoNascimento);
 
 anoAtual = anoAtual + 1;
 console.log(anoAtual);

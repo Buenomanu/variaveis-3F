b04dce938405b411554a0e6a1798d5df97d34940
Buenomanu <129122996+Buenomanu@users.noreply.github.com>
2024-09-02 22:54:32 +0000

    02/09

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const anoNascimento = 2007;
 let anoAtual = 2024;
 const nomeCompleto = "Manuela Bueno";
 
-console.log("Meu nome é " + nomeCompleto  + " nascida no ano de " + anoNascimento);
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
 
 anoAtual = anoAtual + 1;
-console.log(anoAtual);
+console.log(anoAtual)
+idade = idade  + 1;
+console.log(idade)
 
-idade = idade + 1;
-console.log(idade);
+
+//Agência de viagens Vancouver, Seul, Abu Dhabi, Taiwan, Moscou
+
+const listaDeDestinos = new Array(
+  "Vancouver",
+  "Seul",
+  "Abu Dhabi",
+  "Taiwan",
+  "Moscou"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[4]);
+
+listaDeDestinos.push("Coronel Vivida");
+listaDeDestinos.push("Bonito");
+listaDeDestinos.push("Barreirinhas");
+listaDeDestinos.push("Fernando de Noronha");
+
+console.log(listaDeDestinos);
